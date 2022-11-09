19a921babc04c62aea47570013c030b85dea577e
Alex Lambert <hyperactive@live.com>
2022-11-08 16:33:09 -0800

    Add enums/structs to allow mode switching

--- Headgear/Headgear.ino
+++ Headgear/Headgear.ino
@@ -6,33 +6,69 @@
 #define centerTFL 0x11 // I2C address of center TF-Luna
 #define rightTFL 0x10 // I2C address of right TF-Luna
 
-#define leftBuz 4 // pin for left-side buzzer
-#define centerBuz 5 // pin for center buzzer
-#define rightBuz 6 // pin for right buzzer
+#define leftVib 4 // pin for left-side vibration motor
+#define centerVib 5 // pin for center vibration motor
+#define rightVib 6 // pin for right vibration motor
+
+#define leftBuz 7 // pin for left-side buzzer
+#define centerBuz 8 // pin for center buzzer
+#define rightBuz 9 // pin for right buzzer
 
 #define maxDistance 600 // After maxDistance (cm), no buzzer/vibration output
 #define scalar 0.8 // Outermost bucket = 20%, 2nd is outer 20% of remaining, etc.
 #define numBuckets 16 // Number of buckets to categorize distance into
 
-// Focus on 1 distance if it this factor closer than either other distance
-#define closenessFocusFactor 0.5
+// Focus on 1 distance if it is much closer than either other distance
+#define singleClose(dist, distA, distB) ((dist < distA * 0.5) && (dist < distB * 0.5))
+
+enum ActiveMode{
+  off = 0,
+  distance,
+  photocell,
+  compass
+};
+
+enum ActuatorMode {
+  vibration = 0,
+  buzzer,
+  all,
+  none
+};
+
+typedef struct{
+  uint8_t distance;
+  uint8_t vib;
+  uint8_t buz;
+}actuatorSet;
 
 TFLI2C tfl;
 int16_t tfDist;
 int distL, distC, distR;
 unsigned long getDataTimer;
+ActiveMode activeMode;
+ActuatorMode actuatorMode;
+actuatorSet leftSet, centerSet, rightSet;
 
 void setup()
 {
   distL = distC = distR = 0;
   getDataTimer = 0;
+  activeMode = distance;
+
+  actuatorMode = vibration;
+  leftSet.vib = leftVib;
+  centerSet.vib = centerVib;
+  rightSet.vib = rightVib;
+  leftSet.buz = leftBuz;
+  centerSet.buz = centerBuz;
+  rightSet.buz = rightBuz;
 
   //Serial.begin(9600);
   Wire.begin();
 
-  pinMode(leftBuz, OUTPUT);
-  pinMode(centerBuz, OUTPUT);
-  pinMode(rightBuz, OUTPUT);
+  pinMode(leftVib, OUTPUT);
+  pinMode(centerVib, OUTPUT);
+  pinMode(rightVib, OUTPUT);
 
   //pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -41,16 +77,11 @@ void setup()
 void loop()
 {
   unsigned long millisS = millis();
-  //Serial.println(intervalLeft);
-  //Serial.println(intervalCenter);
-  //Serial.println(intervalRight);
-  //Serial.println();
 
   // Collect distance data only from whoever's turn it is
   if (millisS - getDataTimer > 99){
     getDataTimer = millisS;
   } else if (millisS - getDataTimer > 66){
-    // digitalWrite(LED_BUILTIN, LOW);
     if (tfl.getData(tfDist, leftTFL)) {
       distL = tfDist;
       //Serial.print("  left: ");
@@ -63,7 +94,6 @@ void loop()
       //Serial.print(tfDist);
     }
   } else {
-    // digitalWrite(LED_BUILTIN, HIGH);
     if (tfl.getData(tfDist, rightTFL)) {
       distR = tfDist;
       //Serial.print("\tright: ");
@@ -73,20 +103,20 @@ void loop()
 
   // If one is much closer than the other 2, focus on that distance sensor
   bool foundClosePoint = false;
-  if (distL < distC * closenessFocusFactor && distL < distR  * closenessFocusFactor){ // left is closest
-    analogWrite(leftBuz, map(bucketize(distL), 0, 15, 0, 255));
-    digitalWrite(centerBuz, LOW);
-    digitalWrite(rightBuz, LOW);
+  if (singleClose(distL, distC, distR)) { // left is closest
+    analogWrite(leftVib, map(bucketize(distL), 0, 15, 0, 255));
+    digitalWrite(centerVib, LOW);
+    digitalWrite(rightVib, LOW);
     foundClosePoint = true;
-  } else if (distC < distL  * closenessFocusFactor && distC < distR  * closenessFocusFactor) { // center is closest
-    digitalWrite(leftBuz, LOW);
-    analogWrite(centerBuz, map(bucketize(distC), 0, 15, 0, 255));
-    digitalWrite(rightBuz, LOW);
+  } else if (singleClose(distC, distL, distR)) { // center is closest
+    digitalWrite(leftVib, LOW);
+    analogWrite(centerVib, map(bucketize(distC), 0, 15, 0, 255));
+    digitalWrite(rightVib, LOW);
     foundClosePoint = true;
-  } else if (distR < distL  * closenessFocusFactor && distR < distC  * closenessFocusFactor){ // right is closest
-    digitalWrite(leftBuz, LOW);
-    digitalWrite(centerBuz, LOW);
-    analogWrite(rightBuz, map(bucketize(distR), 0, 15, 0, 255));
+  } else if (singleClose(distR, distL, distC)) { // right is closest
+    digitalWrite(leftVib, LOW);
+    digitalWrite(centerVib, LOW);
+    analogWrite(rightVib, map(bucketize(distR), 0, 15, 0, 255));
     foundClosePoint = true;
   }
   /*
@@ -94,28 +124,45 @@ void loop()
     int distAvg = (distL + distC + distR) / 3;
     int distAUL = distAvg * (distAvg * 0.2);
     if (distL < distAUL){
-      analogWrite(leftBuz, map(bucketize(distL), 0, 15, 0, 255)));
+      analogWrite(leftVib, map(bucketize(distL), 0, 15, 0, 255)));
       foundClosePoint = true;
     } else {
-      digitalWrite(leftBuz, LOW);
+      digitalWrite(leftVib, LOW);
     }
     if (distC < distAUL){
-      analogWrite(centerBuz, map(bucketize(distC), 0, 15, 0, 255)));
+      analogWrite(centerVib, map(bucketize(distC), 0, 15, 0, 255)));
       foundClosePoint = true;
     } else {
-      digitalWrite(centerBuz, LOW);
+      digitalWrite(centerVib, LOW);
     }
     if (distR < distAUL){
-      analogWrite(rightBuz, map(bucketize(distR), 0, 15, 0, 255)));
+      analogWrite(rightVib, map(bucketize(distR), 0, 15, 0, 255)));
       foundClosePoint = true;
     } else {
-      digitalWrite(rightBuz, LOW);
+      digitalWrite(rightVib, LOW);
     }
   }*/
   if (!foundClosePoint){
-    analogWrite(leftBuz, map(bucketize(distL), 0, 15, 0, 255));
-    analogWrite(centerBuz, map(bucketize(distC), 0, 15, 0, 255));
-    analogWrite(rightBuz, map(bucketize(distR), 0, 15, 0, 255));
+    analogWrite(leftVib, map(bucketize(distL), 0, 15, 0, 255));
+    analogWrite(centerVib, map(bucketize(distC), 0, 15, 0, 255));
+    analogWrite(rightVib, map(bucketize(distR), 0, 15, 0, 255));
+  }
+}
+
+void actuateDistance(actuatorSet actSet) {
+  switch(actuatorMode) {
+    case vibration:
+      analogWrite(actSet.vib, map(bucketize(actSet.distance), 0, 15, 0, 255));
+      break;
+    case buzzer:
+      analogWrite(actSet.buz, map(bucketize(actSet.distance), 0, 15, 0, 255));
+      break;
+    case all:
+      analogWrite(actSet.vib, map(bucketize(actSet.distance), 0, 15, 0, 255));
+      analogWrite(actSet.buz, map(bucketize(actSet.distance), 0, 15, 0, 255));
+      break;
+    case none:
+      break;
   }
 }
 

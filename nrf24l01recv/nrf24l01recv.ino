8818c88d4b8c8a0de9e4d3da0fa5f6b0cf38fa18
Alex Lambert <hyperactive@live.com>
2022-11-14 14:38:22 -0800

    Made both senders and receivers

--- nrf24l01recv/nrf24l01recv.ino
+++ nrf24l01recv/nrf24l01recv.ino
@@ -7,11 +7,22 @@
 #define INTERVAL_MS_SIGNAL_LOST 1000
 #define INTERVAL_MS_SIGNAL_RETRY 250
 
+/*
+ For Mega:
+ 1 (GND) - GND
+ 2 (VCC) - 3.3V
+ 3 (CE) = pin 9
+ 4 (CSN) = pin 10
+ 5 (SCK) = pin 52
+ 6 (MISO) = pin 50
+ 7 (MOSI) = pin 51
+*/
+
 RF24 radio(CE_PIN, CSN_PIN);
-const byte address[6] = "00001";
+const byte address[][6] = {"00001", "00002"};
 struct payload {
-  byte data1;
-  char data2;
+  uint8_t data1;
+  uint8_t bounceCount;
 };
 payload payload;
 
@@ -25,25 +36,47 @@ void setup() {
   radio.setDataRate(RF24_250KBPS); // transmission rate
   radio.setPALevel(RF24_PA_MAX); // distance and energy consump.
   radio.setPayloadSize(sizeof(payload)); // default 32 bytes
-  radio.openReadingPipe(0, address); // be a receiver
-  radio.startListening();
+
+  radio.openWritingPipe(address[0]);
+  radio.openReadingPipe(1, address[1]);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+  //unsigned long currentMillis = millis();
+  bool readData = false;
   
+  radio.startListening();
   if(radio.available() > 0) {
-    radio.read(&payload, sizeof(payload));
-    Serial.println("Received");
-    Serial.print("Data1:");
+    radio.read(&payload, sizeof(payload)); 
+    readData = true;
+  }
+  radio.stopListening();
+
+  if(readData) {
+    Serial.print("Received: ");
     Serial.println(payload.data1);
-    Serial.print("Data2:");
-    Serial.println(payload.data2);
-    lastSignalMillis = currentMillis;
+    //lastSignalMillis = currentMillis;
+
+    if(bounceCount < 5) {
+      payload.data1 += 10;
+      payload.bounceCount++;
+      Serial.print("Responding with ");
+      Serial.println(payload.data1);
+      radio.write(&payload, sizeof(payload));
+    } else
+      Serial.println("Bounce count too high, no response");
   }
 
-  if(currentMillis - lastSignalMillis > INTERVAL_MS_SIGNAL_LOST) {
+  if(Serial.available()) {
+    payload.data1 = Serial.readString().toInt();
+    payload.bounceCount = 0;
+    Serial.print("\nSending ");
+    Serial.println(payload.data1);
+    radio.write(&payload, sizeof(payload));
+  }
+
+  /*if(currentMillis - lastSignalMillis > INTERVAL_MS_SIGNAL_LOST) {
     Serial.println("We have lost connection, preventing unwanted behavior");
     delay(INTERVAL_MS_SIGNAL_RETRY);
-  }
+  }*/
 }
--- nrf24l01send/nrf24l01send.ino
+++ nrf24l01send/nrf24l01send.ino
@@ -6,11 +6,22 @@
 #define CSN_PIN 10
 #define INTERVAL_MS_TRANSMISSION 250
 
+/*
+ For Uno:
+ 1 (GND) - GND
+ 2 (VCC) - 3.3V
+ 3 (CE) = pin 9
+ 4 (CSN) = pin 10
+ 5 (SCK) = pin 13
+ 6 (MISO) = pin 12
+ 7 (MOSI) = pin 11
+*/
+
 RF24 radio(CE_PIN, CSN_PIN);
-const byte address[6] = "00001";
+const byte address[][6] = {"00001", "00002"};
 struct payload {
-  byte data1;
-  char data2;
+  uint8_t data1;
+  uint8_t bounceCount;
 };
 payload payload;
 
@@ -22,20 +33,41 @@ void setup() {
   radio.setDataRate(RF24_250KBPS); // transmission rate
   radio.setPALevel(RF24_PA_MAX); // distance and energy consump.
   radio.setPayloadSize(sizeof(payload)); // default 32 bytes
-  radio.openWritingPipe(address); // be a transmitter
-  radio.stopListening();
+
+  radio.openWritingPipe(address[1]);
+  radio.openReadingPipe(1, address[0]);
 }
 
 void loop() {
-  payload.data1 = 123;
-  payload.data2 = 'x';
-  radio.write(&payload, sizeof(payload));
+  bool readData = false;
+  
+  radio.startListening();
+  if(radio.available() > 0) {
+    radio.read(&payload, sizeof(payload)); 
+    readData = true;
+  }
+  radio.stopListening();
 
-  Serial.print("Data1:");
-  Serial.println(payload.data1);
-  Serial.print("Data2:");
-  Serial.println(payload.data2);
-  Serial.println("Sent");
+  if(readData) {
+    Serial.print("Received ");
+    Serial.println(payload.data1);
+    
+    if(bounceCount < 5) {
+      payload.data1 += 5;
+      payload.bounceCount++;
+      Serial.print("Responding with ");
+      Serial.println(payload.data1);
+      radio.write(&payload, sizeof(payload));
+    } else
+      Serial.println("Bounce count too high, no response");
+  }
 
-  delay(INTERVAL_MS_TRANSMISSION);
+  if(Serial.available()) {
+    payload.data1 = Serial.readString().toInt();
+    payload.bounceCount = 0;
+    Serial.print("\nSending ");
+    Serial.println(payload.data1);
+    radio.write(&payload, sizeof(payload));
+  }
+  //delay(INTERVAL_MS_TRANSMISSION);
 }
